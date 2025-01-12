ff0186a9bc22cb2f6293fda5bceba68174a50857
lilitantonyan <lilit@helix.am>
2025-01-13 01:19:45 +0400

    add klient page

--- js/main.js
+++ js/main.js
@@ -37,5 +37,75 @@ $(function (){
         });
     });
 
-    observer.observe(document.querySelector('.webmaster__content'));
+    if($('div').hasClass('webmaster__content')){
+
+        observer.observe(document.querySelector('.webmaster__content'));
+    }
+
+    let currentZoom = 0;
+    let currentOpacity = 0;
+    let minZoom = 0;
+    let maxZoom = 1;
+    let stepSize = 0.1;
+
+    $(window).scroll(function (){
+        let direction = 1;
+        console.log($(this).scrollTop());
+        if( $(this).scrollTop() > 1400 ){
+            direction = 1;
+            zoomImage(direction);
+            if($(this).scrollTop() > 1800 &&  $(this).scrollTop() < 2299){
+                $("#clients .clients__right img").css({
+                    'transform':'none',
+                    'position': 'fixed',
+                    'top': '220px'
+                });
+
+            }
+
+            if($(this).scrollTop() >= 2300 ){
+                $("#clients .clients__right img").css({
+                   'transform': 'translateY(880px)',
+                    'position': 'absolute',
+                    'top':'auto'
+                });
+
+            }
+        } else if($(this).scrollTop() < 1399) {
+            $("#clients .clients__right img").css({
+                'transform': 'none',
+            });
+            direction = -1;
+            zoomImage(direction);
+        }
+    })
+    function zoomImage(direction)
+    {
+        let newZoom = currentZoom + direction * stepSize;
+        let newOpacity =  currentOpacity + direction * stepSize;
+
+        if (newZoom < minZoom || newZoom > maxZoom) {
+            return;
+        }
+
+        currentZoom = newZoom;
+        currentOpacity = newOpacity;
+
+        if(direction ===  1){
+            $("#clients .clients__right img").css({
+                'transform': 'scale('+ currentZoom +')',
+                'opacity': currentOpacity,
+                'position': 'fixed',
+                'top': '220px'
+            });
+        } else {
+            $("#clients .clients__right img").css({
+                'transform': 'scale('+ currentZoom +')',
+                'opacity': currentOpacity,
+                'position': 'absolute',
+                'top': 'unset'
+            });
+        }
+
+    }
 })
